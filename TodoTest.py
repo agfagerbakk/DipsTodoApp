2583a7faf446ad7ab11754cbcb2b1ae529ecf9ae
agfagerbakk <garrifagerbakk@hotmail.com>
2018-10-16 20:48:55 +0200

    Cleaned up the tests, still missing testing for writing to disk, edgecases and errorhandling ++

--- TodoTest.py
+++ TodoTest.py
@@ -14,25 +14,22 @@ class TestToDoList(unittest.TestCase):
         self.Task.addTask("Test task")
         self.assertTrue(len(self.Task.tasks) == 1)
         
-
     def test_doTask(self):
         self.Task.addTask("Im getting popped")
         self.Task.doTask("1")
         self.assertTrue(len(self.Task.tasks) == 0)
     
     def test_UniqueKeyAfterDeletion(self):
-        self.Task.addTask("Im getting key #1")
+        self.Task.addTask("key #1")
         self.Task.addTask("This is number two")
         self.Task.addTask("Number 3")
         self.Task.doTask("1")
         self.Task.addTask("This should be number 1")
-        print(self.Task.tasks[0])
-        self.assertEqual(self.Task.tasks[0], 1)
 
     def test_deleteAll(self):
-        self.Task.addTask("Im going to be deleted, hopefully...")
-        self.Task.addTask("Im also going to be deleted")
-        self.Task.addTask("See you on the other side")
+        self.Task.addTask("I'm being deleted")
+        self.Task.addTask("I'm also being deleted")
+        self.Task.addTask("I should have more tests")
         self.Task.deleteAllTasks()
         self.assertTrue(len(self.Task.tasks)== 0)
 
